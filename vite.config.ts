5aec5af19af96f7b7c8a3e3f0ae745fc96557656
Andy Vu <ks.kalo@gmail.com>
2025-03-05 04:29:46 -0500

    added favicon

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
+import svgr from 'vite-plugin-svgr'
+
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,15 +13,31 @@ export default defineConfig({
   base: "/mahjong-flashcards",
   plugins: [
     react(),
+    svgr(),
     tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
       manifest: {
-        name: "Mahjong Tile Flashcards",
-        short_name: "Tiles",
-        description: "An app for learning mahjong tiles",
-        display: "standalone",
-        start_url: "/mahjong-flashcards/?v=342025",
+        "name": "Mahjong Tile Flashcards",
+        "short_name": "MJ Tiles",
+        "icons": [
+          {
+            "src": "/web-app-manifest-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "maskable"
+          },
+          {
+            "src": "/web-app-manifest-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          }
+        ],
+        theme_color: "#ffffff",
+        background_color: "#484848",
+        start_url: `/mahjong-flashcards/?v=${Math.floor(Date.now() / 1000)}`,
+        display: "standalone"
       }
     }),
   ],
